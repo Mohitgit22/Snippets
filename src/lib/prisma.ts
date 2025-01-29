adbd4394f9ae671807d0705c57426531bc24b08e
Mohitgit22 <mohitverma77377@gmail.com>
2025-01-29 19:19:24 +0530

    version fix 9

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,14 @@
-import { PrismaClient } from "../../node_modules/.prisma/client/index";
+// import { PrismaClient } from "../../node_modules/.prisma/client/index";
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
+
+
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

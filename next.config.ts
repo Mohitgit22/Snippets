2da6df2c0081e4ccd8b50e193df2ea16813279b8
Mohitgit22 <mohitverma77377@gmail.com>
2025-01-29 17:41:59 +0530

    version fix 8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      // Remove '@prisma/client' from this list if it's there
+      // You can keep other packages here, if necessary
+    ],
+  },
+  // You can add any other config options you need here
 };
 
 export default nextConfig;

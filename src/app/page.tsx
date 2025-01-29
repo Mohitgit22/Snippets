22a4fd30c6a62731c9921447dbee18657e3e9492
Mohitgit22 <mohitverma77377@gmail.com>
2025-01-29 17:04:59 +0530

    version fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default async function Home() {
       {/* Snippet Cards in Grid Layout */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {snippets.map((snippet) => (
-          <SnippetCard key={snippet.id} id={snippet.id} title={snippet.title} />
+          <SnippetCard key={snippet.id} id={String(snippet.id)} title={snippet.title} />
         ))}
       </div>
 
